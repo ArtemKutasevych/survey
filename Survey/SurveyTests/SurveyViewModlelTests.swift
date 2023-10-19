4dd4da8cf54994de294e4d065e96e358ebef0396
Artem Kutasevych <Artem_Kutasevych@epam.com>
2023-10-19 10:09:33 +0300

    [-] Added unit tests

--- Survey/SurveyTests/SurveyViewModlelTests.swift
+++ Survey/SurveyTests/SurveyViewModlelTests.swift
@@ -42,4 +42,32 @@ final class SurveyViewModelTests: XCTestCase {
         XCTAssertTrue(questionService.getQuestionsCalled)
         XCTAssertTrue(questionService.postAnswerCalled)
     }
+    
+    func testIncrement() {
+        // given
+        let expectedQuestion = Question(id: 2, question: "2 Question")
+        let questions = [Question(id: 1, question: "1 Question"), expectedQuestion]
+        sut?.questions = questions.map { QuestionViewModel(question: $0) }
+        sut?.currentQuestion = QuestionViewModel(question: questions.first ?? expectedQuestion)
+        
+        // when
+        sut?.increment()
+        
+        // then
+        XCTAssertEqual(sut?.currentQuestion.question, expectedQuestion)
+    }
+    
+    func testDecrement() {
+        // given
+        let expectedQuestion = Question(id: 1, question: "1 Question")
+        let questions = [expectedQuestion, Question(id: 2, question: "2 Question")]
+        sut?.questions = questions.map { QuestionViewModel(question: $0) }
+        sut?.currentQuestion = QuestionViewModel(question: questions.last ?? expectedQuestion)
+        
+        // when
+        sut?.decrement()
+        
+        // then
+        XCTAssertEqual(sut?.currentQuestion.question, expectedQuestion)
+    }
 }
